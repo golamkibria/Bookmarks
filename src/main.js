896d61a6f74c89d6590af95a95336a859ea9376e
Kibria <gkibria@dev.panth.com>
2021-05-15 23:50:56 +0600

    sort by then

--- src/main.js
+++ src/main.js
@@ -1,48 +1,52 @@
 // Copyright (c) 2021 gkmobin.net@gmail.com. All rights reserved.
+(function(_){
 
-function sortBookmarks (info, tab){
-	//console.log('callbackHandler: ', info, tab);
-	
-	const url = new URL(tab.url);
-	console.log('Tab.URL:', url, url.hostname);
-	
-	if(url.hostname != 'bookmarks')
-		return;
-	
-	const currentBookmarkNodeId = url.searchParams.get('id');
-	console.log('ContextMenuID', currentBookmarkNodeId)
-	
-	chrome.bookmarks.get(currentBookmarkNodeId, function(node){
-		console.log('BookmarkNode: ', node);
+	function sortBookmarks (info, tab){
+		//console.log('callbackHandler: ', info, tab);
 		
-		chrome.bookmarks.getChildren(currentBookmarkNodeId, function(children){	
+		const url = new URL(tab.url);
+		console.log('Tab.URL:', url, url.hostname);
+		
+		if(url.hostname != 'bookmarks')
+			return;
+		
+		const currentBookmarkNodeId = url.searchParams.get('id');
+		console.log('ContextMenuID', currentBookmarkNodeId)
+		
+		chrome.bookmarks.get(currentBookmarkNodeId, function(node){
+			console.log('BookmarkNode: ', node);
 			
-			const sortedChildren = _(children).chain().sortBy(function(item) {
-				return _.has(item, 'dateGroupModified');
-			}).sortBy(function(item) {
-				return item.title;
-			}).value();
+			chrome.bookmarks.getChildren(currentBookmarkNodeId, function(children){	
 
-			_.each(sortedChildren, function(item, index){
-				console.log(item);				
-				chrome.bookmarks.move(item.id, {index: index, parentId: node.id});
+				//Sort by reverse order
+				var sortedChildren = _(children).chain().sortBy(function(item) {
+					return item.title; 
+				}).sortBy(function(item) {
+					return _.has(item, 'dateGroupModified') ? 0 : 1;
+				}).value();
+	
+				_.each(sortedChildren, function(item, index){
+					console.log(item);				
+					chrome.bookmarks.move(item.id, {index: index, parentId: node.id});
+				});
 			});
 		});
-	});
-}
-
-function setUpContextMenus() {
+	}
 	
-	chrome.contextMenus.create({
-	  title: 'KNET: Bookmarks' ,
-	  id: 'bookmarks@knet.com',
-	  contexts: ['all'],
-	  documentUrlPatterns: ['chrome://bookmarks/*'],
-	  onclick: sortBookmarks
+	function setUpContextMenus() {
+		
+		chrome.contextMenus.create({
+		  title: 'KNET: Bookmarks' ,
+		  id: 'bookmarks@knet.com',
+		  contexts: ['all'],
+		  documentUrlPatterns: ['chrome://bookmarks/*'],
+		  onclick: sortBookmarks
+		});
+	}
+	
+	chrome.runtime.onInstalled.addListener(function(info, tab) {	
+		setUpContextMenus();
+		console.log('KNet:Bookmarks: Initialized');
 	});
-}
 
-chrome.runtime.onInstalled.addListener(function(info, tab) {	
-	setUpContextMenus();
-	console.log('KNet:Bookmarks: Initialized');
-});
+})(_);
